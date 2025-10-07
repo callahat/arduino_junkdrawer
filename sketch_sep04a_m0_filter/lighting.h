e93a73e9387cdb71f0b08e89962029f2c4c6110e
callahat <tim.callahan25@yahoo.com>
2025-10-07 09:55:52 -0400

    Moved the last sample index incrementer to happen in the main function rather than in each of the 4 sub functions Additional debug count to see how often the lights update per second

--- sketch_sep04a_m0_filter/lighting.h
+++ sketch_sep04a_m0_filter/lighting.h
@@ -1,5 +1,6 @@
 // plotter debugging
-#define PLOTTER_LIGHT 1
+// #define DEBUGGING_PLOT_LIGHT 1
+#define DEBUGGING_LIGHT_UPDATES // serial how many light updates per second occur
 
 // pixel strip / light show config
 // reminder, mic is using PIN 2
@@ -7,22 +8,23 @@
 #define N_PIXELS  8  // Number of pixels you are using
 #define TOP N_PIXELS / 4 // effectively number of pixels per band, number of pixels / 4 (since there are 4 bands)
 
-// Averaging the last X samples produces a smoother graph of LED intensities,
+// Averaging the last X samples to produce a smoother graph of LED intensities,
 // LSAMPLES closer to zero can have more rapid bright/off switching strobe effect.
 // Averaging also has a side effect of dampening, so the dampening shifters should be reduced.
+// This should not be too high; the lights only update ~260-270 times a second with this on,
+// and it also seems to add a bit of a lag to the light.
 #define USE_AVERAGE_SAMPLING // Comment this out to disable averaging the samples to get a smoother graph
-#define LSAMPLES 32    //rotating buffer size, power of 2 makes for easy average
-#define LSAMPLE_SHIFT_DIVIDER 5    // 2^n = LSAMPLES
+#define LSAMPLES 8    //rotating buffer size, power of 2 makes for easy average
+#define LSAMPLE_SHIFT_DIVIDER 3    // 2^n = LSAMPLES
 #define LSAMPLES_MASK LSAMPLES - 1 // A quick way to enforce a rolling incremented index
 
 // Effectively use these to throw out low amplitude "noise"
 // Should be adjusted lower when there are more pixels (need to check this, might need more dampening?) and
 //   lower when there are more samples averaged
-// Setting these to 2 seems to be good when averaging with 16 or more samples, otherwis 4 seems a good setting
 // Setting the LOW dampener higher will cut out queiter noises from lighting up the band 
-#define LOWS_DAMPEN_SHIFTER 2
+#define LOWS_DAMPEN_SHIFTER 4
 // Setting the TOP Dampener higher will skew the band in the more "light up" direction
-#define TOPS_DAMPEN_SHIFTER 2
+#define TOPS_DAMPEN_SHIFTER 4
 
 // How many levels of brightness per pixel. Each band is divided into pixels with levels of brightness.
 // Should be a power of two for faster division via bit shifting
@@ -51,9 +53,14 @@ int lSampleIndex = 0; // last sample index
 int tops[4] = {MIN_LVL, MIN_LVL, MIN_LVL, MIN_LVL};
 int lows[4] = {MAX_LVL, MAX_LVL, MAX_LVL, MAX_LVL}; 
 
+#ifdef DEBUGGING_LIGHT_UPDATES
+unsigned long debugLightUpdateMillis;
+int updateLightStripCalls;
+#endif
+
 #include <Adafruit_NeoPixel.h>
 // 8 neopixel strip setup
- Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 // Pebble strand setup
 // Adafruit_NeoPixel strip= Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_BGR + NEO_KHZ800); // pebble coloring
 
@@ -68,9 +75,6 @@ int processSample(byte i, int absy) {
 #if defined USE_AVERAGE_SAMPLING
   filteredSampleSum[i] = filteredSampleSum[i] - filteredSamples[i][lSampleIndex] + absy;
   filteredSamples[i][lSampleIndex] = absy;
-
-  lSampleIndex += 1;
-  lSampleIndex &= LSAMPLES_MASK;
 
   return(filteredSampleSum[i] >> LSAMPLE_SHIFT_DIVIDER);
 #else
@@ -114,8 +118,8 @@ void set_band_color(byte i, int32_t y, uint8_t r, uint8_t g, uint8_t b)
   // quiet samples). 
   int lvl = map(sample>>LOWS_DAMPEN_SHIFTER, lows[i]>>LOWS_DAMPEN_SHIFTER, tops[i]>>TOPS_DAMPEN_SHIFTER, 0, TOP*LVLS_PER_PIXEL);
   
-  #ifdef PLOTTER_LIGHT
-  if(i == 3){
+  #ifdef DEBUGGING_PLOT_LIGHT
+  if(i == 2){
   Serial.print(0);
   Serial.print("\t");
   Serial.print(512);
@@ -143,6 +147,15 @@ void set_band_color(byte i, int32_t y, uint8_t r, uint8_t g, uint8_t b)
 void update_light_strip(int32_t lowest_band, int32_t mid_band, int32_t high_band, int32_t highest_band)
 {
   currentMillis = millis();
+  
+  #ifdef DEBUGGING_LIGHT_UPDATES
+  updateLightStripCalls++;
+  if(currentMillis - 1000 > debugLightUpdateMillis) {
+    Serial.println(updateLightStripCalls);
+    updateLightStripCalls = 0;
+    debugLightUpdateMillis = currentMillis;
+  }
+  #endif
 
   if(currentMillis - decayMillis > DECAY_MS) {
     decayMillis = currentMillis;
@@ -151,6 +164,11 @@ void update_light_strip(int32_t lowest_band, int32_t mid_band, int32_t high_band
       lows[ix] += 1;
     }
   }
+
+  #ifdef USE_AVERAGE_SAMPLING
+  lSampleIndex += 1;
+  lSampleIndex &= LSAMPLES_MASK;
+  #endif
 
   set_band_color(0, lowest_band, 10, 0, 0);
   set_band_color(1, mid_band, 0, 10, 0);
